c10ebd2b726dcbab0f33a38630ad16e1cf88ca90
Gabriel Oliveira <gab.oliveira@outlook.com>
2015-09-26 16:29:31 -0300

    API improved

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var bpiscreen = require('./lib/bpiscreen');
 var config = require('./lib/globals-config');
 
+bpiscreen.start();
+
 bpiscreen.write.led(config.INTERFACELED1, 1);
 bpiscreen.write.led(config.INTERFACELED2, 1);
 bpiscreen.write.led(config.INTERFACELED3, 1);
@@ -8,4 +10,15 @@ bpiscreen.write.led(config.POWERLED, 1);
 bpiscreen.write.led(config.INTERNETLED, 1);
 bpiscreen.write.led(config.BEACONLED, 1);
 
-bpiscreen.write.lcd("Ola mundo!!");
+//bpiscreen.write.lcd("Ola mundo!!");
+
+bpiscreen.write.lcdAt("Oi", 4,0);
+
+var btnTeste = function(err, state){
+	console.log("pressionado");
+	bpiscreen.write.lcd("Botao pressionado");
+}
+
+bpiscreen.watch.button(config.LEFTBTN, btnTeste);
+bpiscreen.watch.button(config.MIDDLEBTN, btnTeste);
+bpiscreen.watch.button(config.RIGHTBTN, btnTeste);
